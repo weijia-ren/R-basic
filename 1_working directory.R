ad3104f1fb1b472d93d8b42d7a1b011b17115262
Weijia <WeijiaRen@westat.com>
2019-09-06 13:15:33 -0400

    updated WR

--- 1_working directory.R
+++ 1_working directory.R
@@ -1,9 +1,9 @@
 # get the current working directory 
 getwd()
 # change/set the working directory
-setwd("C:/Users/ren_w/Desktop/R code/Rmanipulation")      # option 1
-setwd("C:\\Users\\ren_w\\Desktop\\R code\\Rmanipulation") # option 2
+setwd("C:/Users/ren_w/Desktop/R code/R-basic")      # option 1
+setwd("C:\\Users\\ren_w\\Desktop\\R code\\R-basic") # option 2
 
 #### Check the working directory
 dir()  # list files in the wd
-dir.create("C:/Users/ren_w/Desktop/R code/Rmanipulation/data") # create a test folder
+dir.create("C:/Users/ren_w/Desktop/R code/R-basic/data") # create a test folder
