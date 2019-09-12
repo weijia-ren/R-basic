f3c4cd7564e500983079c99d8358501c92b2a0c6
Weijia <WeijiaRen@westat.com>
2019-09-12 10:11:56 -0400

    updated 9.12.19

--- 2_install library.R
+++ 2_install library.R
@@ -1,7 +1,13 @@
 ## Set the library path - because the default folder has too limited space 
 .libPaths(c(.libPaths(),"c:/3.5"))
 
-# install the package 
+## R packages: combination of help documents, datasets, functions
+
+
+# install the package - 1x per computer
 install.packages("tidyverse")
-# call in the package
+# load the package - 1x per R session
 library(tidyverse)
+# tidyverse include: ggplot2, dplyr, tidyr, readr, purrr, tibble, hms, stringr, lubridate, forcats,
+#                   DBI, haven, httr, jsonlite, readxl, rvest, xml2, modelr
+
--- 3_import_export.R
+++ 3_import_export.R
@@ -19,7 +19,7 @@ args(log)  # Description of the command
 ################## Import DATA #################################
 # 1. download dataset from the Internet 
 download.file("http://dss.princeton.edu/training/students.xls",
-              "C:/Users/ren_w/Desktop/R code/Rmanipulation/data/students.xls",
+              "C:/Users/ren_w/Desktop/R code/R-basic/data/students.xlsx",
               method="auto",
               quiet=FALSE,
               mode = "wb",
@@ -27,7 +27,7 @@ download.file("http://dss.princeton.edu/training/students.xls",
 
 
 # 2. from CSV
-csv <- read.csv("C:/Users/ren_w/Desktop/R code/Rmanipulation/data/students.csv", header=TRUE)
+csv <- read.csv("C:/Users/ren_w/Desktop/R code/R-basic/data/students.csv", header=TRUE)
 
 # 3. from TXT
 txt <- read.table("C:/myfolder/abc.txt", header=TRUE, sep="\t", na.strings = "-9") 
@@ -50,9 +50,9 @@ spss <- read.spss("http://dss.princeton.edu/training/mydata.sav",
 library(foreign) 
 sas <- read.xport("c:/myfolder/mydata.xpt") # NOTE: Does not work for files available online
 library(sas7bdat)
-sas <- read.sas7bdat("C:/Users/ren_w/Desktop/R code/Rmanipulation/data/test.sas7bdat") # don't work
+sas <- read.sas7bdat("C:/Users/ren_w/Desktop/R code/R-basic/data/test.sas7bdat") # don't work
 library(haven) 
-sas <- read_sas("C:/Users/ren_w/Desktop/R code/Rmanipulation/data/test.sas7bdat")
+sas <- read_sas("C:/Users/ren_w/Desktop/R code/R-basic/data/test.sas7bdat")
 
 # 7. from R
 load ("C:/../mydata.RData")
@@ -73,7 +73,7 @@ write.foreign(mydata, codefile="test2.sps", datafile="test2.raw", package="SPSS"
 
 # 4. to SAS
 library(haven) 
-write_sas(sas, "C:/Users/ren_w/Desktop/R code/Rmanipulation/data/sas_write.sas7bdat")
+write_sas(sas, "C:/Users/ren_w/Desktop/R code/R-basic/data/sas_write.sas7bdat")
 library(foreign) # didn't try, may not work
 write.foreign(mydata, codefile="test2.sas", datafile="test2.raw", package="SAS") # Provides a syntax file (*.sas) to read the *.raw data file 
 
@@ -81,4 +81,7 @@ write.foreign(mydata, codefile="test2.sas", datafile="test2.raw", package="SAS")
 save.image("mywork.RData") # Saving all objects to file *.RData
 save(object1, object2, file = "mywork.rda") # saving selected objects
 
+# 6. to CSV
+library(tidyverse)
+write_csv(mydata,path="C:/Users/ren_w/Desktop/R code/R-basic/data/csv_write.csv")
 
